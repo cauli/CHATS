a02df345beb4af2ff80d2c2380a7c97dc7ef39f5
cauli <caulitomaz@gmail.com>
2023-12-08 23:57:21 +0100

    Generate User, RoomUser, Message models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :messages
+  resources :room_users
+  resources :users
   resources :rooms
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
