7ac9d5d7250dc02f127c4a029819c668b1364704
cauli <caulitomaz@gmail.com>
2023-12-09 00:48:46 +0100

    List messages on room/:1/show view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,18 @@
 class MessagesController < ApplicationController
+    def create
+        @message = Message.new(message_params)
+        @message.user = current_user
+        @message.room = @room
+        if @message.save
+            redirect_to room_url(@room)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:content)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :messages
   resources :room_users
   resources :users
-  resources :rooms
+  resources :rooms do
+    resources :messages, only: [:create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +12,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "rooms#index"
 end
